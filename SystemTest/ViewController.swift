f7d83028ee3ebc67ce5335cb9770020d0757df92
Rayudu <rayudu_sp2002@yahoo.com>
2020-07-03 13:20:29 +0530

    adding files to git

---
+++ SystemTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SystemTest
+//
+//  Created by Raidu on 7/3/20.
+//  Copyright © 2020 Raidu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
